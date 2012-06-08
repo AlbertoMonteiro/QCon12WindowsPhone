7d1fc4c04cde1109c7444ad91bc10654089cca88
Alberto Monteiro <betimbrasil@gmail.com>
2012-06-07 22:15:59 -0300

    removing trash

--- QCon12.Core/Track.cs
+++
@@ -1,9 +0,0 @@
-namespace QCon12.Core
-{
-    public class Track
-    {
-        public int Id { get; set; }
-        public string Nome { get; set; }
-        public string Bio { get; set; }
-    }
-}
