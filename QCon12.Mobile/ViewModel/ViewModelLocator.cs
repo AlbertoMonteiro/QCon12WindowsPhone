af0a7e44e70edde7767b6585f3de620257ebbeb5
Alberto Monteiro <betimbrasil@gmail.com>
2012-06-12 08:11:10 -0300

    some performace improvements

--- QCon12.Mobile/ViewModel/ViewModelLocator.cs
+++ QCon12.Mobile/ViewModel/ViewModelLocator.cs
@@ -6,39 +6,23 @@ namespace QCon12.Mobile.ViewModel
 {
     public class ViewModelLocator
     {
-        private static MainViewModel _main;
-        private static TrackViewModel _track;
         private static readonly NavigationService navigationService;
-        private static PalestranteViewModel _palestrante;
 
         static ViewModelLocator()
         {
             navigationService = new NavigationService();
-        }
-
-        public ViewModelLocator()
-        {
-            _main = new MainViewModel(ViewModelBase.IsInDesignModeStatic, navigationService);
-            _track = new TrackViewModel(ViewModelBase.IsInDesignModeStatic, navigationService);
-            _palestrante = new PalestranteViewModel(ViewModelBase.IsInDesignModeStatic, navigationService);
+            Main = new MainViewModel(ViewModelBase.IsInDesignModeStatic, navigationService);
+            Track = new TrackViewModel(ViewModelBase.IsInDesignModeStatic, navigationService);
+            Palestrante = new PalestranteViewModel(ViewModelBase.IsInDesignModeStatic, navigationService);
         }
 
         [SuppressMessage("Microsoft.Performance", "CA1822:MarkMembersAsStatic", Justification = "This non-static member is needed for data binding purposes.")]
-        public MainViewModel Main
-        {
-            get { return _main; }
-        }
+        public static MainViewModel Main { get; private set; }
 
         [SuppressMessage("Microsoft.Performance", "CA1822:MarkMembersAsStatic", Justification = "This non-static member is needed for data binding purposes.")]
-        public TrackViewModel Track
-        {
-            get { return _track; }
-        }
+        public static TrackViewModel Track { get; private set; }
 
         [SuppressMessage("Microsoft.Performance", "CA1822:MarkMembersAsStatic", Justification = "This non-static member is needed for data binding purposes.")]
-        public PalestranteViewModel Palestrante
-        {
-            get { return _palestrante; }
-        }
+        public static PalestranteViewModel Palestrante { get; private set; }
     }
 }
